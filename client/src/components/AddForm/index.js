a4fc1c416a39ed6c3d8432d00f3e14c06f0860d0
katiec <katiecampbell21@outlook.com>
2020-11-16 11:18:28 -0500

    image upload box

--- client/src/components/AddForm/index.js
+++ client/src/components/AddForm/index.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 function AddForm() {
 
     const [formData, setFormData] = useState(
-    {
-        location: "",
-        date: "",
-        notes: ""
-    }
+        {
+            location: "",
+            date: "",
+            notes: ""
+        }
     )
 
     const onSubmit = e => {
@@ -23,10 +23,11 @@ function AddForm() {
                     <div className="field">
                         <label className="label">Where to?</label>
                         <div className="control has-icons-left has-icons-right">
-                            <input className="input" type="text" 
-                            onChange={e => {
-                                setFormData({...formData, location: e.target.value});
-                                console.log(formData.location)}} />
+                            <input className="input" type="text"
+                                onChange={e => {
+                                    setFormData({ ...formData, location: e.target.value });
+                                    console.log(formData.location)
+                                }} />
                             <span className="icon is-small is-left">
                                 <i className="fas fa-plane"></i>
                             </span>
@@ -36,10 +37,11 @@ function AddForm() {
                     <div className="field">
                         <label className="label">When?</label>
                         <div className="control has-icons-left">
-                            <input className="input" type="date" 
-                            onChange={e => {
-                                setFormData({...formData, date: e.target.value});
-                                console.log(formData.date)}}/>
+                            <input className="input" type="date"
+                                onChange={e => {
+                                    setFormData({ ...formData, date: e.target.value });
+                                    console.log(formData.date)
+                                }} />
                             <span className="icon is-small is-left">
                                 <i className="fas fa-clock"></i>
                             </span>
@@ -50,23 +52,34 @@ function AddForm() {
                         <label className="label">Notes:</label>
                         <div className="control">
                             <textarea className="textarea" type="text"
-                            onChange={e => {
-                                setFormData({...formData, notes: e.target.value});
-                                console.log(formData.notes)}} >
+                                onChange={e => {
+                                    setFormData({ ...formData, notes: e.target.value });
+                                    console.log(formData.notes)
+                                }} >
                             </textarea>
                         </div>
                     </div>
-                    <div className="field is-grouped">
-                        <div className="control">
-                            <button className="button" onClick={onSubmit}>Add</button>
-                        </div>
-                        <div className="control">
-                            <button className="button is-light">Cancel</button>
+                    <div className="file mb-3 is-link">
+                        <label className="file-label">
+                            <input className="file-input" type="file" name="resume"/>
+                                <span className="file-cta">
+                                    <span className="file-label">
+                                        Choose a file…
+                                        </span>
+                                </span>
+                        </label>
+                    </div>
+                        <div className="field is-grouped">
+                            <div className="control">
+                                <button className="button" onClick={onSubmit}>Add</button>
+                            </div>
+                            <div className="control">
+                                <button className="button is-light">Cancel</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
 
     );
